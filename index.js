1b9fc70ffc606322ced77f6eda16a32cc720b9fb
Pankaj Patil <pankajpatil4949@gmail.com>
2024-09-15 13:08:44 +0530

    git push

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello World!!!")
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (rep, res) => {
+    res.send('<h2>Twitter Id: @Pankajpt40</h2>')
+})
+
+app.get('/youtube', (req, res) => {
+    res.send('<h1>Coming Soon.....!!!</h1>')
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+})
