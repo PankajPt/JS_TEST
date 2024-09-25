b69cfecc54c51d0f6a73ee8a81648fc5fa310337
Pankaj Patil <pankajpatil4949@gmail.com>
2024-09-25 22:45:31 +0530

    edit2

--- calculator/main.js
+++ calculator/main.js
@@ -10,3 +10,13 @@ function getOperator(operator){
     return document.getElementById('display').value += operator;
 }
 
+function backSpace(){
+    let str = document.getElementById('display').value;
+    let len = str.length;
+    let newDisplay = str.slice(0, len - 1 );
+    return document.getElementById('display').value = newDisplay;
+}
+
+function getResult(){
+    return document.getElementById('display').value = "Function cominng sooooon!!!"
+}
