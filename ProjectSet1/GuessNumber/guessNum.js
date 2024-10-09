d980a2405443c064a7981fcaa94a8dfab4013b94
Pankaj Patil <pankajpatil4949@gmail.com>
2024-10-09 15:42:31 +0530

    cosmatic

--- ProjectSet1/GuessNumber/guessNum.js
+++ ProjectSet1/GuessNumber/guessNum.js
@@ -19,13 +19,12 @@ function validateGuess(userInput){
     if (targetNumber == userInput){
         guessHistory.push(userInput);
         guesses.innerHTML = guessHistory;
-        document.querySelector('.lowOrHi').textContent = "You Won";
-        resetTagetNum();
+        document.querySelector('.lowOrHi').textContent = "Victory! You guessed it right!";
     }
     else{
         guessHistory.push(userInput);
         guesses.innerHTML = guessHistory;
-        document.querySelector('.lowOrHi').textContent = "Better luck next time...";
+        document.querySelector('.lowOrHi').textContent = "Almost there! Give it another shot!";
     }
 }
 submit.addEventListener('submit', function(event){
@@ -33,10 +32,9 @@ submit.addEventListener('submit', function(event){
     let userGuess = parseInt(document.getElementById('guessField').value);
     validateGuess(userGuess);
     counter();
-    if (guessesLeft < 0){
+    if (guessesLeft <= 0){
         document.getElementById('subt').disabled = true;
-        document.querySelector('.lowOrHi').textContent = "Maximum attempt reached..";
-        resetTagetNum();
+        document.querySelector('.lowOrHi').textContent = "All attempts exhausted. Try again!";
         console.log("after reset" + targetNumber)
     }
 });
