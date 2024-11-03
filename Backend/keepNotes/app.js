6e78dfe0d750a8ec897f2c05e963fb537e5da170
Pankaj Patil <pankajpatil4949@gmail.com>
2024-11-03 23:00:51 +0530

    keepNotes

---
+++ Backend/keepNotes/app.js
@@ -0,0 +1,22 @@
+console.log('Starting application...')
+const yargs = require('yargs');
+const notes = require('./notes.js')
+
+// const _ = require('lodash');
+// console.log(process.argv)
+// console.log(yArgv);
+
+const userCmd = process.argv[2];
+const yArgv = yargs.argv;
+
+if (userCmd === 'add'){
+    notes.addNote(yArgv.title, yArgv.body)
+} else if( userCmd === 'list' ){
+    notes.getAll()
+} else if( userCmd === 'read' ){
+    notes.getNote(yArgv.title)
+} else if( userCmd === 'remove' ){
+    notes.removeNote(yArgv.title)
+} else {
+    console.log('Command not recognize.')
+}
---
+++ Backend/keepNotes/notes.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+
+// Add Note
+const notesArr = [];
+let addNote = (title, body) => {
+    if ( fs.existsSync('noteData.json')){
+        const data = fs.readFileSync('noteData.json', 'utf-8')
+        try {
+            const parseData = JSON.parse(data)
+            notesArr.push(...parseData)
+            // console.log(notesArr)
+        } catch (error) {
+            console.log(`Error: ${error.message}`)
+        }
+    }
+
+    const note = {
+        title,
+        body
+    }
+    notesArr.push(note)
+    fs.writeFileSync('noteData.json', JSON.stringify(notesArr))
+}
+
+// List all items
+let getAll = () => {
+    console.log('Getting all notes')
+}
+
+// Remove note
+let removeNote = (title) => {
+    console.log(`Remove note with title"${title}"`)
+}
+
+// Read note
+let getNote = (title) => {
+    const note = JSON.parse(fs.readFileSync('noteData.json', 'utf-8'));
+    console.log(`${title}: ${note[0].body}`)
+}
+
+module.exports = {
+    addNote,
+    getAll,
+    removeNote,
+    getNote
+}
